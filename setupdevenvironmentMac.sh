1f9661d8296b0d4e1c2ac94041b26d8174295025
Micah Guttman <memtech3@gmail.com>
2022-11-09 19:15:11 -0600

    attempt at a mac dev environment script

---
+++ setupdevenvironmentMac.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# If you are having trouble running this file try running 'chomd +x setupdevenvironment.sh'
+
+echo "Installing php, unzip, npm and required php extensions"
+brew install php node wget unzip git
+
+cd ../
+echo "Cloning frog-hurricane-skeleton"
+git clone https://github.com/memtech3/frog-hurricane-skeleton
+gravZipName=grav-admin-1.7.37.1.zip
+echo "Downloading grav, saving as $gravZipName"
+wget https://getgrav.org/download/core/grav-admin/1.7.37.1 -O $gravZipName
+echo "Unzipping grav"
+unzip $gravZipName
+
+
+
+currentPath=$(realpath ".")
+gravPath="$currentPath/grav-admin"
+skeletonPath="$currentPath/frog-hurricane-skeleton"
+
+symlinkSkeletonFolder () {
+    echo "Symlinking user/$1" 
+    rm -rf $gravPath/user/$1
+    ln -s $skeletonPath/$1 $gravPath/user/
+}
+
+symlinkSkeletonFolder "accounts"
+symlinkSkeletonFolder "config"
+symlinkSkeletonFolder "pages"
+symlinkSkeletonFolder "plugins"
+
+echo "Symlinking theme" 
+ln -s $currentPath/frog-hurricane $gravPath/user/themes
+
+echo "Installing npm dependencies"
+cd frog-hurricane
+npm install
+
+echo "Making startdevserver.sh executable"
+chmod +x startdevserver.sh
