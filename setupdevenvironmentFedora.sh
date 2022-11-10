7eb59a864cfb150c380d72fb357871bd5e9e43a0
Micah Guttman <memtech3@gmail.com>
2022-11-10 13:14:26 -0600

    Issue #4 add hero to default page template (#6), created distro specific dev environment setup scripts
    created hero partial and included in default page template, created form blueprint for hero and included in default page form blueprint. Hero has title, subtitle, height, color, and background image options
    
    created distro specific dev environment setup scripts
    
    closes #4

---
+++ setupdevenvironmentFedora.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# If you are having trouble running this file try running 'chomd +x setupdevenvironment.sh'
+
+echo "Installing php, unzip, npm and required php extensions"
+sudo dnf update
+sudo dnf install php 
+sudo dnf install php-curl php-dom php-gd php-xml php-zip php-mbstring npm unzip``
+
+cd ../
+echo "Cloning frog-hurricane-skeleton"
+git clone https://github.com/memtech3/frog-hurricane-skeleton
+gravZipName=grav-admin-1.7.37.1.zip
+echo "Downloading grav, saving as $gravZipName"
+wget https://getgrav.org/download/core/grav-admin/1.7.37.1 -O $gravZipName
+echo "Unzipping grav"
+unzip $gravZipName
+
+
+
+currentPath=$(realpath ".")
+gravPath="$currentPath/grav-admin"
+skeletonPath="$currentPath/frog-hurricane-skeleton"
+
+symlinkSkeletonFolder () {
+    echo "Symlinking user/$1" 
+    rm -rf $gravPath/user/$1
+    ln -s $skeletonPath/$1 $gravPath/user/
+}
+
+symlinkSkeletonFolder "accounts"
+symlinkSkeletonFolder "config"
+symlinkSkeletonFolder "pages"
+symlinkSkeletonFolder "plugins"
+
+echo "Symlinking theme" 
+ln -s $currentPath/frog-hurricane $gravPath/user/themes
+
+echo "Installing npm dependencies"
+cd frog-hurricane
+npm install
+
+echo "Making startdevserver.sh executable"
+chmod +x startdevserver.sh
